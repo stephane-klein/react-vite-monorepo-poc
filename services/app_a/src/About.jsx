f4700fee713a5192fc86cd52beb6ced7c45ad6ab
Stéphane Klein <contact@stephane-klein.info>
2022-08-14 22:14:47 +0200

    Setup pnpm workspace

--- services/app_a/src/About.jsx
+++ services/app_a/src/About.jsx
@@ -1,6 +1,7 @@
 import { React } from 'react';
 import { Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import FooBar from 'shared-ui/src/FooBar';
 
 function About () {
     return (
@@ -8,6 +9,9 @@ function About () {
             <p>About page</p>
 
             <Button as={Link} to='/'>Go to Home</Button>
+
+            <hr />
+            <FooBar />
         </div>
     );
 }
---
+++ services/shared-ui/src/FooBar.jsx
@@ -0,0 +1,14 @@
+import { React } from 'react';
+import { Button } from '@chakra-ui/react';
+
+function FooBar () {
+    return (
+        <div>
+            <p>FooBar shared component</p>
+
+            <Button>Click here do nothing</Button>
+        </div>
+    );
+}
+
+export default FooBar;
