6e573021e613638cb7e691e2beb350888946dade
Stéphane Klein <contact@stephane-klein.info>
2022-08-14 23:07:16 +0200

    WIP

--- services/app_a/vite.config.js
+++ services/app_a/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    optimizeDeps: {
+        include: ['shared-ui']
+    },
+    build: {
+        commonjsOptions: {
+            include: [/shared-ui/]
+        }
+    },
     plugins: [react()]
-});
+}
+);
