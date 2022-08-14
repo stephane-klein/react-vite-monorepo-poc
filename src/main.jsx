4d5c5fe2f1679a1652024919e861387be0d80536
Stéphane Klein <contact@stephane-klein.info>
2022-08-14 15:18:23 +0200

    Add Lazy loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Suspense, Fragment, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ChakraProvider, extendTheme, withDefaultColorScheme } from '@chakra-ui/react';
 
-import Home from './Home';
-import About from './About';
+const Home = lazy(() => import('./Home'));
+const About = lazy(() => import('./About'));
 
 const customTheme = extendTheme(withDefaultColorScheme({ colorScheme: 'blue' }));
 
@@ -12,14 +12,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ChakraProvider theme={customTheme}>
             <BrowserRouter>
-                <Switch>
-                    <Route strict exact path="/">
-                        <Home />
-                    </Route>
-                    <Route strict exact path="/about">
-                        <About />
-                    </Route>
-                </Switch>
+                <Suspense fallback={Fragment}>
+                    <Switch>
+                        <Route strict exact path="/">
+                            <Home />
+                        </Route>
+                        <Route strict exact path="/about">
+                            <About />
+                        </Route>
+                    </Switch>
+                </Suspense>
             </BrowserRouter>
         </ChakraProvider>
     </React.StrictMode>
